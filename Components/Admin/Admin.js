c7ae7fa87f6c0b9892560281a02267e56abc6e16
PraveeDurairaj <duraipraveen8834@gmail.com>
2024-08-19 19:33:04 -0700

    monday update

--- Components/Admin/Admin.js
+++ Components/Admin/Admin.js
@@ -12,7 +12,7 @@ function Admin({ planData }) {
                 <h2 className={s.workOutPlanHeader}>{planData[0]?.date}</h2>
                 <Row className='rowGap'>
                     <Col xs={12} sm={12} md={4} lg={4}> <Accordion defaultActiveKey="0">
-                        <Accordion.Item eventKey="0">
+                        <Accordion.Item eventKey="1">
                             <Accordion.Header>Todo</Accordion.Header>
                             <Accordion.Body>
                             <div className={s.taskBoxContainer}>
@@ -24,7 +24,7 @@ function Admin({ planData }) {
                         </Accordion.Item>
                     </Accordion></Col>
                     <Col xs={12} sm={12}  md={4} lg={4}> <Accordion defaultActiveKey="0">
-                        <Accordion.Item eventKey="1">
+                        <Accordion.Item eventKey="0">
                             <Accordion.Header>InProcess</Accordion.Header>
                             <Accordion.Body>
                             <div className={s.taskBoxContainer}>
